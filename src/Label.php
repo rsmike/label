9f2154b9e64ead22aaefd19c0482ef2721fedfc4
Mihail Penkin <rsmike@yandex.ru>
2020-02-18 16:17:29 +0400

    - v1.1 subset functionality

--- src/Label.php
+++ src/Label.php
@@ -10,21 +10,24 @@ namespace rsmike\label;
  * class TestLabel extends Label {
  *     private static $status = [
  *         0 => 'Inactive',
- *         1 => 'Active'
+ *         1 => 'Active',
+ *         2 => 'Other',
  *     ];
  * }
  *
  * General use:
  * echo TestLabel::status(1); // 'Active'
  * echo TestLabel::status(0); // 'Inactive'
- * echo TestLabel::status(2); // 2 (pass-through)
+ * echo TestLabel::status(5); // 5 (pass-through)
  * echo TestLabel::status(null); // null (pass-through)
- * echo TestLabel::status(); // [0=>'Inactive', 1=>'Active']
- * echo TestLabel::status([]); // [0=>'Inactive', 1=>'Active']
- * echo TestLabel::status([0]); // [0, 1] (available keys)
+ * echo TestLabel::status([0, 2]); // [0, 2] (pass-through, invalid input)
+ * echo TestLabel::status(); // [0=>'Inactive', 1=>'Active', 2=>'Other']
+ * echo TestLabel::status([]); // [0=>'Inactive', 1=>'Active', 2=>'Other']
+ * echo TestLabel::status([0]); // [0, 1, 2] (available keys)
+ * echo TestLabel::status([[0, 2]]); // [0=>'Inactive', 1=>'Active', 2=>'Other'] (subset)
  *
  * An additional parameter may be passed as a default value instead of pass-through:
- * echo TestLabel::status(2, 'N/A'); // 'N/A' (key not found)
+ * echo TestLabel::status(5, 'N/A'); // 'N/A' (key not found)
  * echo TestLabel::status(null, 'N/A'); // 'N/A' (always returns default value on null)
  *
  * YN method is a built-in example and may be used in any sub-class
@@ -45,13 +48,19 @@ abstract class Label
             if (is_null($arguments[0])) {
                 return $arguments[1] ?? null;
             }
-            if ($arguments[0] === []) {
-                return static::${$name};
+            if (is_array($arguments[0])) {
+                if ($arguments[0] === []) {
+                    return static::${$name};
+                }
+                if ($arguments[0] === [0]) {
+                    return array_keys(static::${$name});
+                }
+                if (is_array($arguments[0][0])) {
+                    return array_intersect_key(static::${$name}, array_flip($arguments[0][0]));
+                }
+            } else {
+                return static::${$name}[$arguments[0]] ?? $arguments[1] ?? $arguments[0];
             }
-            if ($arguments[0] === [0]) {
-                return array_keys(static::${$name});
-            }
-            return static::${$name}[$arguments[0]] ?? $arguments[1] ?? $arguments[0];
         }
         return static::${$name};
     }
